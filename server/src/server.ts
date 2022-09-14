4256c6f0fe653683f3c91185ee2bbad23a6f9fa5
EliasJuk <eliasp.juk@hotmail.com>
2022-09-14 13:57:54 -0300

    instalação do Prisma ORM e definir rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,39 @@
-import express from 'express'
+import express, { response } from 'express'
 
 const app = express()
 
+app.get('/games', (request, response) => {
+  return response.json([]);
+})
+
 app.get('/ads', (request, response) => {
+  return response.json([]);
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json(['ADS']);
+})
+
+app.get('/games/:id/ads', (request, response) => {
+  //const gameId = request.params.id;
+  //return response.send(gameId);
+
   return response.json([
-    { id: 1, name: 'Anuncio 1'},
-    { id: 2, name: 'Anuncio 2'},
-    { id: 3, name: 'Anuncio 3'},
-    { id: 4, name: 'Anuncio 4'},
-    { id: 5, name: 'Anuncio 5'},
+    { id: 1, name: 'Anuncio 1' },
+    { id: 2, name: 'Anuncio 2' },
+    { id: 3, name: 'Anuncio 3' },
+    { id: 4, name: 'Anuncio 4' },
+    { id: 5, name: 'Anuncio 5' },
   ])
 })
+
+
+app.get('/ads/:id/discord', (request, response) => {
+  //const adId = request.params.id;
+  //return response.send(gameId);
+
+  return response.json([])  
+})
+
 
 app.listen(3333)
