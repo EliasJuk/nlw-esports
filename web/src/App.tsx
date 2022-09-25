20ca941af4acfe7231d0ac8a8ce2c6094cfe9c58
EliasJuk <eliasp.juk@hotmail.com>
2022-09-25 10:38:54 -0300

    estilizacao do modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -113,10 +113,15 @@ function App() {
                     Costumo me conectar ao chat de voz
                   </div>
 
-                  <footer>
-                    <button>Cancelar</button>
-                    <button type="submit">
-                      <GameController />
+                  <footer className='mt-4 flex justify-end gap-4'>
+                    <Dialog.Close type="button" 
+                            className='bg-zinc-500 px-5 h-12 rounded-md font-semibold hover:bg-zinc-600'>
+                      Cancelar
+                    </Dialog.Close >
+
+                    <button type="submit"
+                            className='bg-violet-500 px-5 h-12 rounded-md font-semibold flex items-center gap-3 hover:bg-violet-600'>
+                      <GameController size={24}/>
                       Encontrar duo
                     </button>
                   </footer>
