0e7edddee8c83a1a012be463e7a04ad03941138a
EliasJuk <eliasp.juk@hotmail.com>
2022-09-25 11:02:53 -0300

    carregar dados da API

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
+
 import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
@@ -9,6 +11,14 @@ import { styles } from './styles';
 import { GAMES } from '../../utils/games';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect( () => {
+    fetch('http://192.168.1.7:3333/games')
+      .then(response => response.json())
+        .then(data => setGames(data))
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo}/>
@@ -16,7 +26,7 @@ export function Home() {
       <Heading title='Encontre seu duo!' subtitle='Selecione o game que deseja jogar...'/>
 
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard 
@@ -28,7 +38,6 @@ export function Home() {
         horizontal
         contentContainerStyle={styles.contentList}
       />
-
 
     </View>
   );
